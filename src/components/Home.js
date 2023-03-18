0e7e8c48ca82a02d02db246a2dbf9dfc3bb50bae
hudawakily <hudawakily84@gmail.com>
2023-03-18 13:52:51 +0430

    changed width

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,39 +10,39 @@ import Navbar from './Navbar';
 
 function Home() {
   return (
-    <section>
+    <section className='flex flex-col justify-center items-center lg:h-screen'>
       <Navbar />
-      <div className='flex flex-col justify-center items-center container md:flex-row-reverse lg:flex-row-reverse'>
-        <div className='w-[290px] lg:w-[350px] mt-[100px]'>
+      <div className='flex flex-col justify-center items-center container md:flex-row-reverse lg:flex-row-reverse landing-intro'>
+        <div className='w-[260px] lg:w-[350px] lg:mt-[80px] mt-[100px]'>
           <img src={book} />
         </div>
 
         <div className=' flex flex-col justify-center items-center md:mt-[100px] '>
           <div className='wrapper'>
-            <h1 className=' text-4xl text-white md:ml-[100px] mt-4 refugee'>
+            <h1 className=' text-3xl text-white md:ml-[100px] mt-4 refugee'>
               <span className='ml-[50px] lg:ml-[90px]'>I AM REFUGEE</span>
               <span className='ml-[100px] lg:ml-[155px]' >من مهاجر هستم</span>
-              <span className='ml-[150px] lg:ml-[250px]' >أنا لاجئ</span>
-              <span className='ml-[20px] lg:mr-[50px]'>ESTOU REFUGIADO</span>
-              <span className='ml-[70px] lg:ml-[150px]' >Я БЕЖЕНЦ</span>
+              <span className='ml-[130px] lg:ml-[250px]' >أنا لاجئ</span>
+              <span className='ml-[60px] lg:mr-[50px]'>ESTOU REFUGIADO</span>
+              <span className='ml-[100px] lg:ml-[150px]' >Я БЕЖЕНЦ</span>
             </h1>
           </div>
 
-          <p className=' w-[400px] text-center text-white md:text-right lg:text-right'>A Coleção Estou Refugiado nasceu para contar histórias sobre a luta, as dores e as alegrias das pessoas em situação de refúgio no Brasil. O objetivo é apresentar principalmente a crianças e adolescentes brasileiros – a distribuição dos livros da Coleção privilegia alunos de escolas públicas e privadas – a realidade dessas pessoas que vieram<br /><br /> para o nosso país em busca de mais qualidade de vida, mais liberdade, e dignidade.</p>
-          <div className=' w-[80px] flex flex-col justify-center items-center z-50 pl-0 pr-0 py-4 space-y-4 fixed top-[200px] left-0 ml-[-15px] lg:top-[200px]' id='vector'>
-            <img src={Vector4} />
-            <img src={Vector6} />
-            <img src={Vector5} />
-
-          </div>
-
-          <div className=' flex flex-row space-x-[110px] ml-[55px] mt-[30px]'>
+          <p className=' w-[430px] text-center text-[14px] text-white md:text-right lg:text-right'>A Coleção Estou Refugiado nasceu para contar histórias sobre a luta, as dores e as alegrias das pessoas em situação de refúgio no Brasil. O objetivo é apresentar principalmente a crianças e adolescentes brasileiros – a distribuição dos livros da Coleção privilegia alunos de escolas públicas e privadas – a realidade dessas pessoas que vieram para o nosso país em busca de mais qualidade de vida, mais liberdade, e dignidade.<br /><br />Para o primeiro livro da Coleção, foram escolhidas histórias de refugiadas.</p>
+         
+          <div className=' flex flex-row gap-20 mt-[100px]'>
             <img src={image5} style={{ width: 100 }} />
             <button className='text-white p-3' id='btn'>COMPRE AQUI</button>
+
           </div>
         </div>
 
       </div>
+      <div className='w-[40px] flex flex-col absolute z-50 lg:mt-[-80px] mt-[-300px] ' id='vector'>
+            <img src={Vector4} className="m-[5px]" />
+            <img src={Vector6} className="m-[5px]" />
+            <img src={Vector5} className="m-[5px]" />
+          </div>
     </section>
 
   )
